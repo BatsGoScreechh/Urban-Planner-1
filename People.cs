20bb692389f65bf9a5f5f4e3cdcbc9db030cacee
BatsGoScreechh <bird11@mctc.edu>
2019-05-01 10:35:04 -0400

    Done

---
+++ People.cs
@@ -0,0 +1,12 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Collections;
+
+// namespace urban_planner_1
+// {
+//     public class Buyer
+//     {
+//       public string name {get; set;}
+
+//     }
+// }
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace urban_planner_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building myBuilding = new Building("1239 25th Street"){
+                Width = 15,
+                Depth = 12,
+                Stories = 2
+            };
+            Building railroadSociety = new Building("1323 8th Avenue"){
+                Width = 20,
+                Depth = 30,
+                Stories = 1
+            };
+            Building staffordHouse = new Building("102 Roadway Street"){
+                Width = 20,
+                Depth = 40,
+                Stories = 3
+            };
+
+            myBuilding.Construct(myBuilding);
+            railroadSociety.Construct(railroadSociety);
+            staffordHouse.Construct(staffordHouse);
+
+            myBuilding.Purchase("Dianne Bird");
+            railroadSociety.Purchase("Railroad Society");
+            staffordHouse.Purchase("John Williams");
+
+            // Console.WriteLine($"{myBuilding.address}");
+            myBuilding.Print();
+            railroadSociety.Print();
+            staffordHouse.Print();
+        }
+    }
+}
